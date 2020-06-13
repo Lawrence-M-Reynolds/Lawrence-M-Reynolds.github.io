0b2250ba73bb4891551ac8891d79ddbc63b5b4ed
Lawrence-M-Reynolds <lawrence.m.reynolds@googlemail.com>
2020-06-13 23:03:32 +0100

    pushing build dir

---
+++ build/precache-manifest.a341d1e37851325d18dfd0222f595d63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc9c3c395b4479e2c37a0da6f7af195",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "fba91a70d30d188c5859",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/react-gh-pages/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fba91a70d30d188c5859",
+    "url": "/react-gh-pages/static/js/main.ed12279f.chunk.js"
+  },
+  {
+    "revision": "6ff939d621f4b1c08e3f",
+    "url": "/react-gh-pages/static/js/runtime-main.39256a51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
